28934521b234dc646079fd9bfe754a5ced40af49
Owen Bickford <owen+github@owencode.com>
2022-08-13 22:28:39 -0400

    add challenge generation

--- lib/components/form.ex
+++ lib/components/form.ex
@@ -7,8 +7,9 @@ defmodule WebAuthnLiveComponent.Form do
   import Phoenix.LiveView.Helpers
   alias Ecto.Changeset
 
-  # prop changeset, :struct
-  # prop user, :map
+  # prop app, :atom, required: true
+  # prop changeset, :struct, default: build_changeset()
+  # prop username, :string
   # prop params, :map
   # prop css_class, :css_class
   # prop register_label, :string
@@ -19,7 +20,6 @@ defmodule WebAuthnLiveComponent.Form do
       :ok,
       socket
       |> assign_new(:changeset, &build_changeset/1)
-      |> assign_new(:id, fn -> "passwordless_auth" end)
       |> assign_new(:css_class, fn -> "grid gap-2 grid-cols-2" end)
       |> assign_new(:register_label, fn -> "Sign Up" end)
       |> assign_new(:authenticate_label, fn -> "Sign In" end)
@@ -93,7 +93,7 @@ defmodule WebAuthnLiveComponent.Form do
     }
   end
 
-  def handle_event("register", _payload, %{assigns: %{changeset: changeset}} = socket) do
+  def handle_event("register", _payload, %{assigns: %{changeset: changeset, app: app}} = socket) do
     %{changes: %{username: username}} = changeset
 
     new_changeset =
@@ -101,13 +101,27 @@ defmodule WebAuthnLiveComponent.Form do
       |> build_changeset()
       |> add_changeset_requirements()
 
-    send(self(), {:register_user, username: username})
+    challenge = build_registration_challenge(socket)
+
+    challenge_data =
+      build_challenge_data(challenge, %{app: app, username: username})
+      |> IO.inspect(label: :challenge_data)
 
     {
       :noreply,
       socket
       |> assign(:changeset, new_changeset)
+      |> assign(:challenge, challenge)
+      |> push_event("registration_challenge", challenge_data)
     }
+  end
+
+  def handle_event("register_attestation", payload, %{assigns: %{changeset: changeset}} = socket) do
+    %{changes: %{username: username}} = changeset
+    IO.inspect(payload, label: :register_attestation)
+    send(self(), {:register_user, username: username})
+
+    {:noreply, socket}
   end
 
   def handle_event("authenticate", _payload, %{assigns: %{changeset: changeset}} = socket) do
@@ -140,5 +154,29 @@ defmodule WebAuthnLiveComponent.Form do
     changeset
     |> Changeset.validate_required([:username])
     |> Changeset.validate_length(:username, min: 3, max: 40)
+  end
+
+  defp build_registration_challenge(socket) do
+    Wax.new_registration_challenge(
+      attestation: "none",
+      origin: get_origin(socket),
+      rp_id: :auto
+    )
+  end
+
+  defp build_challenge_data(challenge, %{app: app, username: username} = _params) do
+    %{
+      appName: app,
+      attestation: challenge.attestation,
+      challenge_64: Base.encode64(challenge.bytes, padding: false),
+      rp_id: challenge.rp_id,
+      user: %{username: username},
+      user_verification: challenge.user_verification
+    }
+  end
+
+  defp get_origin(socket) do
+    %{scheme: scheme, host: host} = socket.host_uri
+    "#{scheme}://#{host}"
   end
 end
