00e2be88c25e117c73903ad20ba731dea0e54024
Owen Bickford <owen+github@owencode.com>
2022-08-13 21:18:16 -0400

    add wax dep

--- mix.exs
+++ mix.exs
@@ -34,10 +34,11 @@ defmodule WebauthnLiveComponent.MixProject do
     [
       {:phoenix, "~> 1.6"},
       {:phoenix_live_view, "~> 0.17"},
-      {:jason, "~> 1.0", only: [:dev, :test]},
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:jason, "~> 1.0"},
+      {:ex_doc, "~> 0.24", only: [:dev], runtime: false},
       {:phoenix_ecto, "~> 4.4"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:wax_, "~> 0.4"}
     ]
   end
 
