869d51293f3487839460d6c6757eb6e50c689bb5
Owen Bickford <owen+github@owencode.com>
2022-08-13 21:00:12 -0400

    update tests

--- test/components/form_test.exs
+++ test/components/form_test.exs
@@ -1,6 +1,7 @@
 defmodule WebAuthnLiveComponent.FormTest do
   use ComponentCase
   alias WebAuthnLiveComponent.Form
+  import Phoenix.LiveView.Helpers
   doctest Form
 
   test "socket assigns include a valid changeset on mount" do
@@ -12,9 +13,10 @@ defmodule WebAuthnLiveComponent.FormTest do
     assert changeset.valid?
   end
 
-  test "form rendering" do
-    component = Form.render(%{})
-    assert %Phoenix.LiveView.Rendered{static: [static]} = component
-    assert static =~ "form"
+  test "a form is rendered" do
+    live_component = live_component(Form, id: "test_form")
+    assert %Phoenix.LiveView.Component{component: component, assigns: assigns} = live_component
+    assert %Phoenix.LiveView.Rendered{} = component.render(assigns)
+    assert render_component(Form, id: "test_form") =~ "username"
   end
 end
