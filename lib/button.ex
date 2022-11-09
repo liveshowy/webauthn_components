4dcce702b0af45cc1a318576c5042874aea457b5
Owen Bickford <owen+github@owencode.com>
2022-11-08 21:18:19 -0600

    add button component

---
+++ lib/button.ex
@@ -0,0 +1,17 @@
+defmodule WebAuthnLiveComponent.Button do
+  use Phoenix.Component
+
+  attr :label, :string,
+    default: "Sign in a Passkey",
+    doc: "Text to be rendered inside the button, visible to the user"
+
+  attr :class, :string, default: "px-4 p-2 bg-blue-500 text-white rounded shadow-md"
+
+  def render(assigns) do
+    ~H"""
+    <button class={@class}>
+      <%= @label %>
+    </button>
+    """
+  end
+end
