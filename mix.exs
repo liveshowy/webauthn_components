4cc4b9b6b15e1cd368400cd8d0a1cdf97ddc61af
Owen Bickford <type1fool@users.noreply.github.com>
2023-01-28 20:47:36 -0600

    Fix button text (#40)
    * add focus state
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule WebauthnComponents.MixProject do
 
   # Don't forget to change the version in `package.json`
   @source_url "https://github.com/liveshowy/webauthn_components"
-  @version "0.3.1"
+  @version "0.3.2"
 
   def project do
     [
