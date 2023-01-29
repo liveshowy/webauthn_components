514a480ce800590795e722327d3542f609d0b5a7
Owen Bickford <owen@owencode.com>
2023-01-28 20:33:47 -0600

    wip

--- lib/webauthn_components/base_components.ex
+++ lib/webauthn_components/base_components.ex
@@ -8,7 +8,7 @@ defmodule WebauthnComponents.BaseComponents do
 
   attr :class, :string,
     default: """
-    px-2 py-1 border border-gray-300 dark:border-gray-600 hover:border-transparent bg-gray-200 hover:bg-blue-600 hover:text-white focus:text-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 transition rounded text-base shadow-sm flex gap-2 items-center hover:-translate-y-px hover:shadow-md
+    px-2 py-1 border border-gray-300 dark:border-gray-600 hover:border-transparent bg-gray-200 hover:bg-blue-600 focus:bg-blue-700 hover:text-white focus:text-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 transition rounded text-base shadow-sm flex gap-2 items-center hover:-translate-y-px hover:shadow-md
     """
 
   attr :rest, :global
