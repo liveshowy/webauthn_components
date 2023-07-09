ada98bd92a33c81ce398262d0fccd1008fc0d644
Owen Bickford <owen@owencode.com>
2023-07-09 15:53:26 -0500

    copy schemas into subdirs

--- lib/wac_gen/schemas.ex
+++ lib/wac_gen/schemas.ex
@@ -18,9 +18,10 @@ defmodule Wac.Gen.Schemas do
   def copy_template(template, assigns) when template in @templates and is_list(assigns) do
     app_snake_case = Keyword.fetch!(assigns, :app_snake_case)
     file_name = @template_files[template]
+    dir_name = to_string(template) <> "s"
     template_dir = Path.expand("../../templates/schemas", __DIR__)
     source = Path.join([template_dir, file_name])
-    target = Path.join(["lib", app_snake_case, file_name])
+    target = Path.join(["lib", app_snake_case, dir_name, file_name])
     Mix.Generator.copy_template(source, target, assigns)
   end
 end
