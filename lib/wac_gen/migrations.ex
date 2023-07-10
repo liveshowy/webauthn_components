355f34c3cd7eda93d48b9b3153e58634338a948c
Owen Bickford <owen@owencode.com>
2023-07-09 19:11:11 -0500

    fix: rename migrations

--- lib/wac_gen/migrations.ex
+++ lib/wac_gen/migrations.ex
@@ -4,20 +4,23 @@ defmodule Wac.Gen.Migrations do
   """
 
   @template_files %{
-    users: "users.ex",
-    user_keys: "user_keys.ex",
-    user_tokens: "user_tokens.ex"
+    users: "users.exs",
+    user_keys: "user_keys.exs",
+    user_tokens: "user_tokens.exs"
   }
 
   @templates Map.keys(@template_files)
 
   def copy_templates(assigns) do
-    for template <- Enum.sort(@templates), do: copy_template(template, assigns)
+    for template <- @templates do
+      copy_template(template, assigns)
+      Process.sleep(1_000)
+    end
   end
 
   def copy_template(template, assigns) when template in @templates and is_list(assigns) do
     file_name = @template_files[template]
-    timestamp = NaiveDateTime.local_now() |> Calendar.strftime("%Y%m%d%H%M%S")
+    timestamp = NaiveDateTime.utc_now(:second) |> Calendar.strftime("%Y%m%d%H%M%S")
     timestamped_file_name = "#{timestamp}_#{file_name}"
     template_dir = Path.expand("../../templates/migrations/", __DIR__)
     source = Path.join([template_dir, file_name])
