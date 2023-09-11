30c838a61ec00718469f5ce9e4a9786b84b50514
Owen Bickford <owen@owencode.com>
2023-09-10 22:34:34 -0500

    add link to slack convo

--- lib/wac_gen/router.ex
+++ lib/wac_gen/router.ex
@@ -4,6 +4,8 @@ defmodule Wac.Gen.Router do
   def update_router(assigns) do
     web_snake_case = Keyword.fetch!(assigns, :web_snake_case)
     router_path = Path.join(["lib", web_snake_case, "router.ex"])
+    # Slack request for help:
+    # https://elixir-lang.slack.com/archives/C03EPRA3B/p1694398121220679
     IO.puts("Skipping #{router_path} for now")
   end
 end
