b222e34c2b352b5feaf751707f5296fb4d580ce4
Owen Bickford <owen@owencode.com>
2023-09-12 20:37:33 -0500

    extract functions, improve error handling

--- lib/wac_gen/router.ex
+++ lib/wac_gen/router.ex
@@ -1,32 +1,43 @@
+defmodule Wac.Gen.RouterError do
+  defexception [:message]
+
+  @impl true
+  def exception(message) do
+    formatted_message =
+      """
+      🫣 #{message}
+
+      🙏 Please review the WebauthnComponents issue tracker and open a new issue if necessary.
+      👉 https://github.com/liveshowy/webauthn_components/issues
+      👇 For debugging, please include this error and the stacktrace below:
+      """
+
+    %__MODULE__{message: formatted_message}
+  end
+end
+
 defmodule Wac.Gen.Router do
   @moduledoc false
   alias Sourceror.Zipper
 
+  @format_opts [
+    locals_without_parens: [
+      plug: :*,
+      get: :*,
+      scope: :*,
+      delete: :*,
+      live: :*,
+      post: :*,
+      forward: :*,
+      live_dashboard: :*,
+      live_session: :*,
+      pipe_through: :*
+    ]
+  ]
+
   def update_router(assigns) do
     web_snake_case = Keyword.fetch!(assigns, :web_snake_case)
     router_path = Path.join(["lib", web_snake_case, "router.ex"])
-
-    # Child AST trees to be inserted
-    aliases = aliases(assigns)
-    plug_fetch_current_user = plug_fetch_current_user()
-    session_routes = session_routes(assigns)
-    guest_routes = guest_routes(assigns)
-    authenticated_routes = authenticated_routes(assigns)
-
-    format_opts = [
-      locals_without_parens: [
-        plug: :*,
-        get: :*,
-        scope: :*,
-        delete: :*,
-        live: :*,
-        post: :*,
-        forward: :*,
-        live_dashboard: :*,
-        live_session: :*,
-        pipe_through: :*
-      ]
-    ]
 
     modified_router_contents =
       router_path
@@ -34,20 +45,54 @@ defmodule Wac.Gen.Router do
       |> Sourceror.parse_string!()
       |> Zipper.zip()
       |> Zipper.find(&find_aliases/1)
-      |> Zipper.insert_right(aliases)
+      |> insert_aliases(assigns)
       |> Zipper.find(&find_browser_pipeline/1)
-      |> Zipper.insert_right(plug_fetch_current_user)
+      |> insert_plugs()
       |> Zipper.find(&find_first_scope/1)
-      |> Zipper.insert_right(authenticated_routes)
-      |> Zipper.insert_right(guest_routes)
-      |> Zipper.insert_right(session_routes)
+      |> insert_routes(assigns)
       |> Zipper.root()
-      |> Sourceror.to_string(format_opts)
+      |> Sourceror.to_string(@format_opts)
 
     File.write!(router_path, modified_router_contents)
+    status = IO.ANSI.red() <> "* updating " <> IO.ANSI.reset() <> router_path
+    IO.puts(status)
+  end
 
-    # Code.format_file!(router_path)
-    IO.puts("Updated #{router_path}")
+  defp insert_aliases(%Zipper{} = zipper, assigns) do
+    aliases = aliases(assigns)
+
+    zipper
+    |> Zipper.insert_right(aliases)
+  end
+
+  defp insert_aliases(nil, _assigns) do
+    raise Wac.Gen.RouterError, "Unable to find aliases"
+  end
+
+  defp insert_plugs(%Zipper{} = zipper) do
+    plug_fetch_current_user = plug_fetch_current_user()
+
+    zipper
+    |> Zipper.insert_right(plug_fetch_current_user)
+  end
+
+  defp insert_plugs(nil) do
+    raise Wac.Gen.RouterError, "Unable to find the browser pipeline"
+  end
+
+  defp insert_routes(%Zipper{} = zipper, assigns) do
+    session_routes = session_routes(assigns)
+    guest_routes = guest_routes(assigns)
+    authenticated_routes = authenticated_routes(assigns)
+
+    zipper
+    |> Zipper.insert_right(authenticated_routes)
+    |> Zipper.insert_right(guest_routes)
+    |> Zipper.insert_right(session_routes)
+  end
+
+  defp insert_routes(nil, _assigns) do
+    raise Wac.Gen.RouterError, "Unable to find the first router scope"
   end
 
   defp find_aliases({:use, _meta, [{_, _, _}, {:__block__, _, [:router]} | _]}), do: true
@@ -67,55 +112,66 @@ defmodule Wac.Gen.Router do
   defp aliases(assigns) do
     web_pascal_case = assigns[:web_pascal_case]
 
-    quote do
-      alias unquote(web_pascal_case).SessionHooks.AssignUser
-      alias unquote(web_pascal_case).SessionHooks.RequireUser
-      import unquote(web_pascal_case).Session, only: [fetch_current_user: 2]
-    end
+    """
+    alias #{inspect(web_pascal_case)}.SessionHooks.AssignUser
+    alias #{inspect(web_pascal_case)}.SessionHooks.RequireUser
+    import #{inspect(web_pascal_case)}.Session, only: [fetch_current_user: 2]
+    """
+    |> Sourceror.parse_string!()
   end
 
   defp plug_fetch_current_user do
-    quote do
+    """
       plug :fetch_current_user
-    end
+    """
+    |> Sourceror.parse_string!()
   end
 
   defp session_routes(assigns) do
-    quote do
-      # HTTP controller routes
-      scope "/", unquote(assigns[:web_pascal_case]) do
-        pipe_through :browser
+    web_pascal_case = assigns[:web_pascal_case]
 
-        post "/session", Session, :create
-        delete "/session", Session, :delete
-      end
+    """
+    # HTTP controller routes
+    scope "/", #{inspect(web_pascal_case)} do
+      pipe_through :browser
+
+      post "/session", Session, :create
+      delete "/session", Session, :delete
     end
+    """
+    |> Sourceror.parse_string!()
   end
 
   defp guest_routes(assigns) do
-    quote do
-      # Unprotected LiveViews
-      live_session :guest, on_mount: [AssignUser] do
-        scope "/", unquote(assigns[:web_pascal_case]) do
-          pipe_through :browser
+    web_pascal_case = assigns[:web_pascal_case]
 
-          live "/sign-in", AuthenticationLive
-        end
+    """
+    # Unprotected LiveViews
+    live_session :guest, on_mount: [AssignUser] do
+      scope "/", #{inspect(web_pascal_case)} do
+        pipe_through :browser
+
+        live "/sign-in", AuthenticationLive
       end
     end
+    """
+    |> Sourceror.parse_string!()
   end
 
   defp authenticated_routes(assigns) do
-    quote do
-      # Protected LiveViews
-      live_session :authenticated, on_mount: [AssignUser, RequireUser] do
-        scope "/", unquote(assigns[:web_pascal_case]) do
-          pipe_through :browser
+    web_pascal_case = assigns[:web_pascal_case]
 
-          # Example
-          # live "/room/:room_id", RoomLive
-        end
+    """
+    # Protected LiveViews
+    live_session :authenticated, on_mount: [AssignUser, RequireUser] do
+      scope "/", #{inspect(web_pascal_case)} do
+        pipe_through :browser
+
+        # Example
+        # live "/room/:room_id", RoomLive
       end
     end
+    """
+    |> Sourceror.parse_string!()
   end
 end
