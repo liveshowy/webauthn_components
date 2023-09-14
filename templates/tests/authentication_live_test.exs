d2da2a0822de6a141d7ededac20dab247815836e
Owen Bickford <owen@owencode.com>
2023-09-13 23:50:57 -0500

    add test placeholders

--- templates/tests/authentication_live_test.exs
+++ templates/tests/authentication_live_test.exs
@@ -22,10 +22,23 @@ defmodule <%= inspect @web_pascal_case %>.AuthenticationLiveTest do
       assert has_element?(view, "form fieldset#fieldset-registration")
       assert has_element?(view, "[phx-hook='AuthenticationHook'][phx-click='authenticate']")
 
-      # Session Form
-      assert has_element?(view, "form#token-form[action='/session'][method='post'].hidden")
-      assert has_element?(view, "form#token-form input[type='text'][name='value']")
-      assert has_element?(view, "form#token-form button[type='submit']")
+      # Token/Session Form
+      # This form is only rendered when registration or authentication is successful.
+      refute has_element?(view, "form#token-form[action='/session'][method='post'].hidden")
+      # assert has_element?(view, "form#token-form input[type='text'][name='value']")
+      # assert has_element?(view, "form#token-form button[type='submit']")
     end
+  end
+
+  describe "handle_event: update-form" do
+  end
+
+  describe "handle_info: passkeys_supported" do
+  end
+
+  describe "handle_info: registration_successful" do
+  end
+
+  describe "handle_info: find_credentials" do
   end
 end
