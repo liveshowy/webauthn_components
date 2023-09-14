8810e29a07709261ed1fe493c0610a2c4c4e129a
Owen Bickford <owen@owencode.com>
2023-09-13 23:37:57 -0500

    update regex, add status message

--- lib/wac_gen/javascript.ex
+++ lib/wac_gen/javascript.ex
@@ -1,6 +1,9 @@
 defmodule Wac.Gen.Javascript do
   @moduledoc false
 
+  @import_regex Regex.compile!("(import { LiveSocket } from \"phoenix_live_view\";)")
+  @socket_regex Regex.compile!("(params: { _csrf_token: csrfToken })")
+
   @import_hooks """
   import {
     SupportHook,
@@ -13,15 +16,14 @@ defmodule Wac.Gen.Javascript do
 
   def inject_hooks do
     javascript_path = Path.join(["assets", "js", "app.js"])
+    status = IO.ANSI.red() <> "* updating " <> IO.ANSI.reset() <> javascript_path
+    IO.puts(status)
 
     updated_contents =
       javascript_path
       |> File.read!()
-      |> String.replace(
-        ~r/(import { LiveSocket } from "phoenix_live_view";)/,
-        "\g{1}\n#{@import_hooks}"
-      )
-      |> String.replace(~r/(params: { _csrf_token: csrfToken })/, "\g{1},\n#{@socket_hooks}")
+      |> String.replace(@import_regex, "\g{1}\n#{@import_hooks}")
+      |> String.replace(@socket_regex, "\g{1},\n#{@socket_hooks}")
 
     File.write!(javascript_path, updated_contents)
   end
