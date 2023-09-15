ac350782639ceb5742faab05e9351b56a5468f8f
Owen Bickford <owen@owencode.com>
2023-09-14 20:31:02 -0500

    inject sign-in link to root

--- lib/wac_gen/app_html.ex
+++ lib/wac_gen/app_html.ex
@@ -22,4 +22,30 @@ defmodule Wac.Gen.AppHtml do
     <#{inspect(web_pascal_case)}.NavigationComponents.navbar current_user={@current_user} />
     """
   end
+
+  @flash_string "<.flash_group flash={@flash} />"
+
+  def update_root_html(assigns) do
+    web_snake_case = Keyword.fetch!(assigns, :web_snake_case)
+    file_path = Path.join(["lib", web_snake_case, "components", "layouts", "app.html.heex"])
+    home_link = home_link(assigns)
+    injected_string = "#{@flash_string}\n#{home_link}"
+
+    modified_file_contents =
+      file_path
+      |> File.read!()
+      |> String.replace(@flash_string, injected_string)
+
+    File.write!(file_path, modified_file_contents)
+  end
+
+  defp home_link(assigns) do
+    web_pascal_case = Keyword.fetch!(assigns, :web_pascal_case)
+
+    """
+    <#{inspect(web_pascal_case)}.NavigationComponents.nav_link href={~p"/sign-in"}>
+      Sign In
+    </#{inspect(web_pascal_case)}.NavigationComponents.nav_link>
+    """
+  end
 end
