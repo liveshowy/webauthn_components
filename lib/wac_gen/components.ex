36b84e4218f533e2a0e2eb365cd02e6833a4aef3
Owen Bickford <owen@owencode.com>
2023-09-17 15:37:43 -0500

    adjust default flash placement

--- lib/wac_gen/components.ex
+++ lib/wac_gen/components.ex
@@ -13,6 +13,8 @@ defmodule Wac.Gen.Components do
 
   def copy_templates(assigns) do
     for template <- @templates, do: copy_template(template, assigns)
+
+    update_flash_component(assigns)
   end
 
   def copy_template(template, assigns) when template in @templates and is_list(assigns) do
@@ -28,5 +30,23 @@ defmodule Wac.Gen.Components do
       Mix.Generator.copy_template(source, target, assigns)
       Code.format_file!(target)
     end
+  end
+
+  def update_flash_component(assigns) do
+    # This could use `Sourceror.Zipper`, but the AST for the component
+    # and its `~H` would still require string replacement.
+    # If the core components are revised or redesigned, this would fail silently.
+
+    web_snake_case = Keyword.fetch!(assigns, :web_snake_case)
+    search_string = "fixed top-2 right-2 w-80"
+    replacement_string = "fixed top-14 right-2 w-80"
+    file_path = Path.join(["lib", web_snake_case, "components", "core_components.ex"])
+
+    updated_content =
+      file_path
+      |> File.read!()
+      |> String.replace(search_string, replacement_string)
+
+    File.write!(file_path, updated_content)
   end
 end
