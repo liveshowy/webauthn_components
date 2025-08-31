a4a9cac46aad467ae691993415d2ca5f1bb99f61
James Gough <james@peacefulprogramming.dev>
2025-08-31 22:17:39 +0100

    stop icons shrinking with long button labels

--- lib/webauthn_components/icon_components.ex
+++ lib/webauthn_components/icon_components.ex
@@ -20,7 +20,7 @@ defmodule WebauthnComponents.IconComponents do
       viewBox="0 0 24 24"
       stroke-width="1.5"
       stroke="currentColor"
-      class="w-full h-full"
+      class="w-full h-full min-w-4 min-h-4"
     >
       <path
         stroke-linecap="round"
@@ -39,7 +39,7 @@ defmodule WebauthnComponents.IconComponents do
       fill="currentColor"
       viewBox="0 0 24 24"
       stroke="currentColor"
-      class="w-full h-full"
+      class="w-full h-full min-w-4 min-h-4"
     >
       <path d="M 23.5,12 L 21,10 L 21,11 L 8,11 L 10.5,6.5 C 10.75,6 11.125,5.5 11.5,5.5 C 12.75,5.5 13.25,5.5 13.5,5.5 C 13.75,7 14.5,8 15.5,8 C 16.75,8 17.5,7 17.5,5.5 C 17.5,4 16.75,3 15.5,3 C 14.5,3 13.75,4 13.5,5.5 L 11.5,5.5 C 11,5.5 10.25,6.25 10,6.75 L 7,11 L 5,11 C 4.75,9 3.5,7.5 2,7.5 C 0.75,7.5 0,9.25 0,12 C 0,14.75 0.75,16.5 2,16.5 C 3.5,16.5 4.75,15 5,13 L 7,13 L 10,17.25 C 10.25,17.75 11,18.5 11.5,18.5 L 13.5,18.5 L 13.5,20 L 16,20 L 16,16 L 13.5,16 L 13.5,17.5 L 11.5,17.5 C 11.125,17.5 10.75,17 10.5,16.5 L 8,13 L 21,13 L 21,14 L 23.5,12 z" />
     </svg>
