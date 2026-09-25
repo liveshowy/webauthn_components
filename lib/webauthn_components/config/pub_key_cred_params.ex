ec2f7f0b606e64e548e9ed090a10f3e909fb6577
Owen Bickford <owen@owencode.com>
2026-09-25 18:18:35 -0500

    Set default pub key cred params, document common values

--- lib/webauthn_components/config/pub_key_cred_params.ex
+++ lib/webauthn_components/config/pub_key_cred_params.ex
@@ -2,9 +2,20 @@ defmodule WebauthnComponents.Config.PubKeyCredParams do
   @moduledoc """
   Struct used to specify the algorithms supported by the host application.
 
+  ## Algorithms
+
+  By default, `WebauthnComponents.Config.PublicKeyOptions` includes the algorithms suggested by MDN documentation:
+
+  - `-8` EdDSA
+  - `-7` ES256
+  - `-257` RS256
+
+  You may override the default list of `PubKeyCredParams` when creating the `PublicKeyOptions` struct.
+
   ## Resources
 
   - https://developer.mozilla.org/en-US/docs/Web/API/PublicKeyCredentialCreationOptions#pubkeycredparams
+  - https://www.iana.org/assignments/cose#algorithms
   """
 
   @type t :: %__MODULE__{
