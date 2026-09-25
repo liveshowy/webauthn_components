e100e03e170606afdad2f292fd9d63828254462d
Owen Bickford <owen@owencode.com>
2026-09-25 18:18:56 -0500

    Update RegistrationComponent and RegistrationHook

--- priv/static/registration_hook.ts
+++ priv/static/registration_hook.ts
@@ -4,88 +4,18 @@ export const RegistrationHook = {
   mounted() {
     console.info(`RegistrationHook mounted`);
 
-    if (this.el.dataset.check_uvpa_available) {
-      this.checkUserVerifyingPlatformAuthenticatorAvailable(this, {
-        errorMessage: this.el.dataset.uvpa_error_message,
-      });
-    }
-
-    this.handleEvent("registration-challenge", (event) => {
-      if (event.id == this.el.id) {
-        this.handleRegistration(event, this);
+    this.handleEvent("registration-challenge", async ({ id, publicKey }) => {
+      if (id == this.el.id) {
+        try {
+          publicKey.challenge = base64ToArray(publicKey.challenge).buffer;
+          publicKey.user.id = base64ToArray(publicKey.user.id);
+          const credential = await navigator.credentials.create({ publicKey });
+          this.pushEventTo(this.el, "credential", credential);
+        } catch (error) {
+          console.error(error);
+          handleError(error, this);
+        }
       }
     });
-  },
-  async checkUserVerifyingPlatformAuthenticatorAvailable(
-    context,
-    { errorMessage }
-  ) {
-    if (
-      !(await window.PublicKeyCredential?.isUserVerifyingPlatformAuthenticatorAvailable())
-    ) {
-      const error = new Error(errorMessage);
-      error.name = "NoUserVerifyingPlatformAuthenticatorAvailable";
-      handleError(error, context);
-      throw error;
-    }
-  },
-
-  async handleRegistration(event, context) {
-    try {
-      const {
-        attestation,
-        authenticatorAttachment,
-        challenge,
-        excludeCredentials,
-        residentKey,
-        requireResidentKey,
-        rp,
-        timeout,
-        user,
-      } = event;
-      const challengeArray = base64ToArray(challenge);
-
-      user.id = base64ToArray(user.id).buffer;
-
-      const publicKey: PublicKeyCredentialCreationOptions = {
-        attestation,
-        authenticatorSelection: {
-          authenticatorAttachment: authenticatorAttachment,
-          residentKey: residentKey,
-          requireResidentKey: requireResidentKey,
-        },
-        challenge: challengeArray.buffer,
-        excludeCredentials,
-        pubKeyCredParams: [
-          { alg: -7, type: "public-key" },
-          { alg: -257, type: "public-key" },
-        ],
-        rp,
-        timeout,
-        user,
-      };
-
-      const credential = (await navigator.credentials.create({
-        publicKey,
-      })) as PublicKeyCredential;
-      if (credential) {
-        const { rawId, response, type } = credential;
-        const { attestationObject, clientDataJSON } =
-          response as AuthenticatorAttestationResponse;
-        const attestation64 = arrayBufferToBase64(attestationObject);
-        const clientData = Array.from(new Uint8Array(clientDataJSON));
-        const rawId64 = arrayBufferToBase64(rawId);
-
-        context.pushEventTo(context.el, "registration-attestation", {
-          attestation64,
-          clientData,
-          rawId64,
-          type,
-        });
-      }
-    } catch (error) {
-      console.error(error);
-      handleError(error, context);
-    }
   },
 };
